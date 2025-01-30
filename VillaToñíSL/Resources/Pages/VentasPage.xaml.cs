cf5ade519f92de33d8153e9e8b6f643176bcf3ba
Yeray <yerayblanco@hotmail.com>
2025-01-30 21:37:02 +0100

    ventas formulario

--- VillaToñíSL/Resources/Pages/VentasPage.xaml.cs
+++ VillaToñíSL/Resources/Pages/VentasPage.xaml.cs
@@ -24,11 +24,11 @@ public partial class VentasPage : ContentPage
         // Datos de ventas (simulados)
         var ventas = new List<ChartEntry>
             {
-                new ChartEntry(5000) { Label = "Enero", ValueLabel = "5000", Color = SKColor.Parse("#3498db") },
+                new ChartEntry(1000) { Label = "Enero", ValueLabel = "5000", Color = SKColor.Parse("#3498db") },
                 new ChartEntry(7000) { Label = "Febrero", ValueLabel = "7000", Color = SKColor.Parse("#2ecc71") },
-                new ChartEntry(6500) { Label = "Marzo", ValueLabel = "6500", Color = SKColor.Parse("#f1c40f") },
+                new ChartEntry(500) { Label = "Marzo", ValueLabel = "6500", Color = SKColor.Parse("#f1c40f") },
                 new ChartEntry(8000) { Label = "Abril", ValueLabel = "8000", Color = SKColor.Parse("#e74c3c") },
-                new ChartEntry(9000) { Label = "Mayo", ValueLabel = "9000", Color = SKColor.Parse("#9b59b6") }
+                new ChartEntry(30) { Label = "Mayo", ValueLabel = "9000", Color = SKColor.Parse("#9b59b6") }
             };
 
         // Configurar gráfico de barras
@@ -45,6 +45,11 @@ public partial class VentasPage : ContentPage
 
 
 
+    }
+
+    private void OnGuardarVentaClicked(object sender, EventArgs e)
+    {
+        
     }
 }
 
